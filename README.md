76de68ca691a57b5c82f364bb14d83e25c16a4d8
Robin Aggleton <robinaggleton@gmail.com>
2014-11-22 15:08:45 +0100

    First working version (C++ only) with packaged extension.

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 #github-linkify
 
-This is a Chrome extension to turn `#include X` and `import X`/`from X import Y` statments into hyperlinks, so you can find the files easily. (I don't know why this hasn't been done already.)
-
-Designed initially to work with CMSSW. YMMV. Not working? Log and Issue.
+This Chrome extension turns `#include X` in C/C++ files (and in the future `import X`/`from X import Y` in python configs) into hyperlinks when viewing CMSSW files on Github.com, so you can get to the includes easily. (I don't know why this hasn't been done already.)
 
 ## Instructions
 
 1) First clone me:
 ```
-git clone 
+git clone git@github.com:raggleton/github-linkify.git
 ```
 2) Then install me:
+    - go to `chrome://extensions`
+    - drag and drop the `github-linkify.crx` package onto the window. This should install it. (not tested, please feedback)
 
+## TODO
+- [ ] Do for Python `import`/`from X import Y` statements
+- [ ] ignore STL/boost libraries (like `<iostream>`)
+- [ ] keep the colour of the links to their original colour
 
 Maybe I'll do a Firefox equivalent some day. If I ever get this one done.
 
---
+++ contentscript.js
@@ -0,0 +1,46 @@
+// get current page URL
+url = table.baseURI;
+// make root URL, i.e. only up to CMSSW_*
+var pattern = /CMSSW_[0-9]_[0-9]_[0-9]_[A-Za-z0-9]*/;
+cmssw = pattern.exec(url)[0]; // gives us the CMSSW name
+rootURL = url.substr(0, url.search(pattern)+cmssw.length+1);
+// console.log("URL: " + rootURL);
+
+// pattern to do #include matching
+cpp_pattern = /#include/;
+cpp_header_pattern = /[<\"].*[>\"]/;
+
+// the lines of code are stored in a table
+table = document.getElementsByClassName("js-file-line-container")[0];
+
+// loop through all rows <tr>
+var rows = document.getElementsByTagName("tr");
+for (var i = 0; i < rows.length; i++) {
+    for (var j = 0; j < rows[i].childNodes.length; j++) {
+
+        // check it's a line of the file, not anything else
+        var file_line_pattern = /js-file-line/;
+        var cell = rows[i].childNodes[j];
+        if (file_line_pattern.test(cell.className)) {
+            var line = cell.textContent;
+
+            // check line has a C++ #include, if so pull the header path
+            // TODO: check it's not a STL include, like iostream
+            if (cpp_pattern.test(line)) {
+                var path = cpp_header_pattern.exec(line)[0];
+                path = path.replace(/[<>\"]/g,""); //g inmportant - global so replaces all instances
+                var link = rootURL.concat(path);
+
+                // let's replace the text with a link
+                // TODO keep same styling as before
+                for (var k = 0; k < cell.childNodes.length; k++) {
+                    if (cell.childNodes[k].textContent.indexOf(path) != -1) {
+                        var cell_html = cell.childNodes[k].innerHTML;
+                        cell.childNodes[k].innerHTML = cell_html.replace(path, "<a href=\""+link+"\">"+path+"</a>");
+                    }
+                }
+            }
+            // TODO test for python
+        }
+    }
+}
